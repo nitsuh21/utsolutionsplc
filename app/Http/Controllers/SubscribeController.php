1ff9ccb6105a3b6b48b7607ea935ea0cd17bd3fd
unknown <blenderthesky12@gmail.com>
2022-02-28 17:03:35 +0000

    mak

--- app/Http/Controllers/SubscribeController.php
+++ app/Http/Controllers/SubscribeController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\subscriber;
+use App\Models\Subscriber;
 class SubscribeController extends Controller
 {
     public function index(Request $request){
